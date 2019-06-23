f6b033f8527befa6399f92714255ba1098eb63c1
プチいちご <PetitStrawberry@users.noreply.github.com>
2019-06-23 18:33:02 +0900

    Added more sileo native items to webview pages

--- cydia/script/depictions.js
+++ cydia/script/depictions.js
@@ -30,20 +30,33 @@ function html_from_depiction(fileName) {
       h = h + '<ul><li>';
     };
     for (var k in depiction[i].views) {
-      if (typeof depiction[i].views[k].title !== 'undefined') {
-        var title = depiction[i].views[k].title
-        if (depiction[i].views[k].class == 'DepictionHeaderView') {
-          h = h + '<hr><h3>' + title + '</h3>';
-        };
-        if (depiction[i].views[k].class == 'DepictionSubheaderView') {
-          h = h + '<h4>' + title + '</h4>';
-        };
-      };
+    
       if (typeof depiction[i].views[k].markdown !== 'undefined') {
         h = h + depiction[i].views[k].markdown;
-      };
-      if (depiction[i].views[k].class == 'DepictionSeparatorView') {
-        h = h + '<hr>';
+      };     
+      if (typeof depiction[i].views[k].class !== 'undefined') {
+        if (depiction[i].views[k].class == 'DepictionHeaderView') {
+         h = h + '<hr><h3>' + depiction[i].views[k].title + '</h3>';
+        };
+        if (depiction[i].views[k].class == 'DepictionSubheaderView')  {
+          h = h + '<h4>' + depiction[i].views[k].title + '</h4>';
+        };
+        if (depiction[i].views[k].class == 'DepictionTableTextView') {
+          h = h + '<tr><td style="color: #999;">'+ depiction[i].views[k].title + '</td><td align="right">' + depiction[i].views[k].text + '</td></tr>';
+        };
+        if (depiction[i].views[k].class == 'DepictionTableButtonView') {
+          h = h + '<li><a href="'+depiction[i].views[k].action+'" target="_blank" role="button">'+ depiction[i].views[k].title + '</a></li>';
+        };
+        if (depiction[i].views[k].class == 'DepictionSeparatorView') {
+          h = h + '<hr>';
+        };
+        if (depiction[i].views[k].class == 'BeginingOfTable') {
+          h = h + '<table width="100%">';
+        };
+        if (depiction[i].views[k].class == 'EndOfTable') {
+          h = h + '</table>';
+        };
+        
       };
     };
     h = h + '</li></ul>';

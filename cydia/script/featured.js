2021cc57ea108584f81ae60d734b6fa5b274f4f7
PetitStrawberry <petitstrawberry.dev@gmail.com>
2019-08-12 23:41:27 +0900

    Improve FuturedTweaks

--- cydia/script/featured.js
+++ cydia/script/featured.js
@@ -1,12 +1,20 @@
 function html_from_depiction(package) {
     var depiction = obj_from_json(readTextFile("sileodepictions/" + package + ".json"));
-    var title = depiction.title
-    var detail = depiction.description
+    var title = depiction.title;
+    var detail = depiction.description;
+    var price = depiction.price;
     var h = `
             <li>
-                <a href="depictions.html?package=`+ package + `" role="button">
-                    `+ title + `
-                    <p>`+ detail + `</p>
+                <a href="depictions.html?package=`+ package + `"  role="button">
+                    <table width="100%">
+                        <tr>
+                            <td>`+ title + `</td>
+                            <td align="left" rowspan="2"><p style="color: #999; font-size: 13px;">` + price + `</p></td>
+                        </tr>
+                        <tr>
+                            <td><p>` + detail + `</p></td>
+                        </tr>
+                    </table>
                 </a>
             </li>
             `;
